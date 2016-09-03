a0625b3cc95c22c3e2e9717596b559c3bb68d281
Reetu kainulainen <rkainulainen@gmail.com>
2016-09-03 16:01:06 +0300

    bugfixes

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ slapp.message('streamers', (msg) => {
 
 slapp.command('/add', /.*/, (msg, text) => {
 	streamers.concat(',ESL_GO');
-   	msg.respond('Awesome! Now Im watching '+{msg.body.text}+)
+   	msg.respond('Awesome! Now Im watching ESL_GO')
 })
 
 require('beepboop-slapp-presence-polyfill')(slapp, { debug: true })
