d27bee60a916c58201413c5563e7e0f08bece46f
zikzxak <rkainulainen@gmail.com>
2016-09-04 18:53:59 +0300

    help

--- flows/help.js
+++ flows/help.js
@@ -2,33 +2,33 @@
 
 module.exports = (slapp) => {
 
-  let help = `In or Out is pretty simple. Ask question with the \`/inorout\` command:
-\`\`\`
-/inorout [type your question here]
-[answer 1]
-[answer 2]
-[...]
-\`\`\`
+  let help = `Twitchy will let you know when your favorite streamers are online.
 
-Up to 15 answers may go on following lines (shift-enter or ctrl-enter or return on mobile).
-
-For example:
+Commands:
 
 \`\`\`
-/inorout What time should we meet?
-10:30AM PST
-2:00PM PST
-:no_entry: never
+/add [channel]
 \`\`\`
 
-Choose a button option and results are aggregated under the question.
+\`\`\`
+/remove [channel]
+\`\`\`
 
-":arrow_heading_down: move to bottom" moves the question down to the bottom of the stream.
+\`\`\`
+/list
+\`\`\`
 
-Like this! https://goo.gl/ucnthN
+\`\`\`
+/monitor start
+\`\`\`
+
+\`\`\`
+/monitor stop
+\`\`\`
+
 `
 
-  slapp.command('/inorout', /^\s*help\s*$/, (msg) => {
+  slapp.command('/twitchy', /^\s*help\s*$/, (msg) => {
     msg.respond(help)
   })
 
