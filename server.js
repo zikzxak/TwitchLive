9e59b5cf710a6366bfa43ccf09f80b1a6326f721
zikzxak <rkainulainen@gmail.com>
2016-09-04 12:41:58 +0300

    maius breaks

--- server.js
+++ server.js
@@ -6,130 +6,129 @@ const BeepBoopContext = require('slapp-context-beepboop')
 const axios = require('axios')
 const TwitchApi = require('twitch-api')
 if (!process.env.PORT) throw Error('PORT missing but required')
-
+ 
 var slapp = Slapp({
   record: 'out.jsonl',
   convo_store: BeepBoopConvoStore(),
   context: BeepBoopContext()
 })
-
+ 
 var twitch = new TwitchApi({})
-
+ 
 var streamers = []
-
-var monitoring = false
-
-slapp.message('goodnight', ['direct_mention', 'direct_message'], (msg, text) => {
+ 
+var monitoringTimeout = null;
+ 
+slapp.message('goodnight', ['direct_mention', 'direct_message'], (msg) => {
   msg.say('sweet dreams :crescent_moon: ')
 })
-
+ 
 slapp.command('/monitor', /^\s*start\s*$/, (msg) => {
-	monitoring = true;
-	msg.say('Started monitoring streams!')
-	console.log('getting streams')
-	function getStreams() {
-		console.log('monitoring...');
-		if(streamers.length > 0) {
-			streamers.forEach(function(streamer, index) {
-				console.log(streamer.name + ': ' + streamer.streaming);
-				twitch.getChannelStream(streamer.name, function(err, body) {
-					if (err) {
-						console.log(err);
-					} else if (body.stream) {
-						if (streamers[index].streaming === false) {
-							msg.say(body.stream.channel.display_name + ' is playing ' + body.stream.game + '.' + ' Stream: ' + body.stream.channel.url);
-							streamers[index].streaming = true
-						}
-					} else if (streamers[index].streaming === true ) {
-						streamers[index].streaming = false
-					}
-				});
-			});
-		} else {
-			console.log('no streamers!')
-		}
-		if (monitoring === true) {
-			setTimeout(getStreams, 30000);
-		} else {
-			return;
-		}
-		
-	}
-
-	getStreams();
+    msg.say('Started monitoring streams!')
+    console.log('getting streams')
+    function getStreams() {
+        console.log('monitoring...');
+        //no streamers
+        if(!streamers.length){
+            return;
+        }
+ 
+        streamers.forEach(function(streamer, index) {
+            console.log(streamer.name + ': ' + streamer.streaming);
+            twitch.getChannelStream(streamer.name, function(err, body) {
+                if (err) {
+                    console.log(err);
+                } else if (body.stream) {
+                    if (streamers[index].streaming == false) {
+                        msg.say(body.stream.channel.display_name + ' is playing ' + body.stream.game + '.' + ' Stream: ' + body.stream.channel.url);
+                        streamers[index].streaming = true
+                    }
+                } else if (streamers[index].streaming == true ) {
+                    streamers[index].streaming = false
+                }
+            });
+        });
+    }
+ 
+    monitoringTimeout = setTimeout(getStreams, 30000);
+    getStreams();
 });
-
+ 
 slapp.command('/monitor', /^\s*stop\s*$/, (msg) => {
-
-	monitoring = false;
+    //no timeout, dont clear
+    if(!monitoringTimeout){
+        return;
+    }
+ 
+    clearTimeout(monitoringTimeout);
 });
-
+ 
 slapp.command('/add', /.*/, (msg, text) => {
-	var streamer = {name: text.trim().toLowerCase(), streaming: false}
-	console.log(streamer);
-	if(streamers.length > 0) {
-		streamers.forEach(function(name, index) {
-			if (name.name === text.trim().toLowerCase()) {
-				msg.say('Im already watching' + text + '!')
-				return;
-			} else if (index === streamers.length - 1) {
-				streamers.push(streamer)
-				msg.respond('Awesome! Now Im watching ' + text)
-				console.log('added streamer: ' + streamer.name);
-			}
-		});
-	} else {
-		streamers.push(streamer)
-		msg.respond('Awesome! Now Im watching ' + text)
-		console.log('added streamer: ' + streamer.name);
-	}
-   	console.log(streamers);
+    var streamer = {name: text.trim().toLowerCase(), streaming: false}
+    console.log(streamer);
+    if(streamers.length > 0) {
+        streamers.forEach(function(name, index) {
+            if (name.name == text.trim().toLowerCase()) {
+                msg.say('Im already watching' + text + '!')
+                return;
+            } else if (index == streamers.length - 1) {
+                streamers.push(streamer)
+                msg.respond('Awesome! Now Im watching ' + text)
+                console.log('added streamer: ' + streamer.name);
+            }
+        });
+    } else {
+        streamers.push(streamer)
+        msg.respond('Awesome! Now Im watching ' + text)
+        console.log('added streamer: ' + streamer.name);
+    }
+    console.log(streamers);
 })
-
+ 
 slapp.command('/delete', /.*/, (msg, text) => {
-	var streamer = text.trim().toLowerCase()
-	console.log(streamer);
-	if(streamers.length > 0) {
-		streamers.forEach(function(name, index) {
-			if (name.name === streamer) {
-				msg.say('Sad to see ' + streamer + ' go :(')
-				streamers.splice(index, 1);
-				return;
-			} else if (index === streamers.length - 1) {
-				msg.say('w00t I couldnt find ' + streamer + ' !??')
-			}
-		});
-	} else {
-		msg.say('w00t I couldnt find ' + streamer + ' !??')
-	}
-   	console.log(streamers);
+    var streamer = text.trim().toLowerCase()
+    console.log(streamer);
+    if(streamers.length > 0) {
+        streamers.forEach(function(name, index) {
+            if (name.name == streamer) {
+                msg.say('Sad to see ' + streamer + ' go :(')
+                streamers.splice(index, 1);
+                return;
+            } else if (index == streamers.length - 1) {
+                msg.say('w00t I couldnt find ' + streamer + ' !??')
+            }
+        });
+    } else {
+        msg.say('w00t I couldnt find ' + streamer + ' !??')
+    }
+    console.log(streamers);
 })
-
+ 
 slapp.command('/list', (msg) => {
-	if(streamers.length > 0) {
-		streamers.forEach(function(name, index) {
-			twitch.getChannelStream(name.name, function(err, body) {
-				if (err) {
-					console.log(err);
-				} else if (body.stream) {
-					msg.say(body.stream.channel.display_name + ' is playing ' + body.stream.game + '.' + ' Stream: ' + body.stream.channel.url);
-				} else {
-					msg.say(name.name + ' is offline');
-				}
-			});
-		});
-	} else {
-		msg.respond('no streamers on my watch!')
-	}
-})		
-
+    if(streamers.length > 0) {
+        streamers.forEach(function(name, index) {
+            twitch.getChannelStream(name.name, function(err, body) {
+                if (err) {
+                    console.log(err);
+                } else if (body.stream) {
+                    msg.say(body.stream.channel.display_name + ' is playing ' + body.stream.game + '.' + ' Stream: ' + body.stream.channel.url);
+                } else {
+                    msg.say(name.name + ' is offline');
+                }
+            });
+        });
+    } else {
+        msg.respond('no streamers on my watch!')
+    }
+})     
+ 
 require('beepboop-slapp-presence-polyfill')(slapp, { debug: true })
 require('./flows')(slapp)
 var app = slapp.attachToExpress(express())
-
+ 
 app.get('/', function (req, res) {
   res.send('Hello')
 })
-
+ 
 console.log('Listening on :' + process.env.PORT)
 app.listen(process.env.PORT)
